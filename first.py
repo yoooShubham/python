044db79e574d06ae9ecb854138e6c8d9d13ccffb
shubham acharya <shubham772883@gmail.com>
2025-08-04 08:49:42 -0700

    logic P

--- first.py
+++ first.py
@@ -1,7 +1,20 @@
-print("hello world !")
-print("enter your name :")
-hello =input()
-print(hello,"bonjur")
-name="shubham"
+# print("hello world !")
+# print("enter your name :")
+# hello =input()
+# print(hello,"bonjur")
+# name="shubham"
 
-print(hello,"bonjur"+name)
+# print(hello,"bonjur"+name)
+
+# comaprision operators 
+# >
+# >
+# !
+# <=
+# >=
+
+# logical operator 
+if(not 10 > 20):
+    print(True)
+else:
+    print(False)
