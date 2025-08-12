3d140c324924fbf725289a005f0502df71b71e4f
shubham <shubham772883@gmail.com>
2025-08-12 22:53:59 +0530

    add some QA

--- first.py
+++ first.py
@@ -28,3 +28,19 @@
 # num1= float(input("enter num1"))
 # num2= float(input("enter num2"))
 # print("average is :",(num1+num1)/2)
+# # idx in strings 
+
+# str="hello world"
+# str="yoo"
+# print(str[ :])
+# str1= str[4:7]
+# print(str1)
+# print(str)
+
+# wap to input user name and its length  
+# Fname=input("enter your first name: ")
+# print("and this is your name length: ",len(Fname))
+
+# wap to find $ in string 
+# stri ="aug$dudhd#$^"
+# print(stri.count("$"))
